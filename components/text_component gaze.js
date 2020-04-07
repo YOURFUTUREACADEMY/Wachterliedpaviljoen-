05ec3832f9c9a728df2f19c9b9297827a581b6d9
Daniel <daniel@web-reality.nl>
2020-04-07 14:32:58 +0200

    test3

--- components/text_component gaze.js
+++ components/text_component gaze.js
@@ -1,110 +1,70 @@
-< !DOCTYPE html >
-    <
-    html lang = "en" >
+AFRAME.registerComponent('cursor-listener', {
 
-    <
-    head >
-    <
-    meta charset = "UTF-8" >
-    <
-    meta name = "viewport"
-content = "width=device-width, initial-scale=1.0" >
-    <
-    script src = "https://aframe.io/releases/1.0.4/aframe.min.js" > < /script> <
-    script src = "https://unpkg.com/aframe-event-set-component@5.0.0/dist/aframe-event-set-component.min.js" > < /script> <
-    script src = "components/text_component.js" > < /script> <
-    link rel = "stylesheet"
-type = "text/css"
-href = "css/style.css" >
-    <
-    title > Wachterliedpaviljoen goes digital - DEMO < /title>
+    schema: {
+        title: {
+            type: 'string',
+            default: 'Here will be HTML formated title '
+                // Title of the text
+        },
+        content: {
+            type: 'string',
+            default: 'Here will be HTML formated text '
+                // The text it self
+        },
+        image: {
+            type: 'asset',
+            default: ''
+                // The image file
+        },
+        imageAlignment: {
+            type: 'string',
+            default: 'right'
+                // Position of the image in the modal
+        }
+    },
 
+    init: function() {
 
+    },
+    update: function() {
+        const textTitle = this.data.title; // Get the title from data
+        const textContent = this.data.content; // Get the text content from data
+        const image = this.data.image; // Get the image src from data
+        const imagePos = this.data.imageAlignment; // Get the image alignment from data
+        const modal = document.getElementById('textModal'); // Get the modal
+        const closeBtn = document.getElementById("closeBtn"); // Get the modal
 
+        // When the user clicks the button, open the modal and show the title, text, image and placement of the image
+        this.el.addEventListener('click', function(evt) {
+            document.getElementById("modal-image").src = image;
+            document.getElementById("modal-image").onload = function() {
+                document.getElementById("modal-title").innerHTML = textTitle;
+                document.getElementById("modal-text").innerHTML = textContent;
+                if (imagePos === "left") {
+                    document.getElementById("flex-inner").style.flexDirection = "row-reverse";
+                } else {
+                    document.getElementById("flex-inner").style.flexDirection = "row";
+                }
+                modal.style.display = "block";
+                closeBtn.style.display = "block";
+            };
 
-<
-body >
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target === modal) {
+                    hidemodal();
+                }
+            };
+        });
+        // When the user clicks on <closeBtn> (x), close the modal
+        closeBtn.addEventListener('click', function(evt) {
+            hidemodal();
+        });
+        //Function to hide the modal
+        function hidemodal() {
+            modal.style.display = "none";
+            closeBtn.style.display = "none";
+        }
 
-
-    <
-    a - scene >
-    <
-    a - assets >
-    <
-    img id = "paveljoen"
-src = "./img/Wachterliedpaviljoen.jpg" >
-    <
-    img id = "logo"
-src = "./img/thuismaker.jpg" >
-    <
-    /a-assets> <
-    a - sky src = "#paveljoen" > < /a-sky>
-
-<
-a - entity position = "0 1.6 0"
-camera look - controls wasd - controls cursor = "rayOrigin: mouse" > < /a-entity> <
-    a - entity position = "0 0 -5" >
-    <
-    a - entity id = "myBtn1"
-geometry = "primitive: sphere;  radius: 1.5; "
-rotation = "0 0 0"
-position = "0 0 -3"
-material = "src: #logo"
-animation = "property: rotation; to: 0 360 0; loop: true; dur: 10000"
-content = "title:Welkom bij het Wachterliedpaviljoen!; content:Dit is de demo voor de digitale VR omgeving; image: img/logo.jpg; imageAlignment:right" >
-    <
-    /a-entity>
-
-<
-/a-scene> <
-div id = "textModal"
-class = "modal " >
-    <
-    span id = "closeBtn"
-class = "close" > & times; < /span> <
-div class = "modal-content " >
-    <
-    div id = "flex-inner"
-class = "flex-box" >
-    <!--Flexbox is easy to to switch the order (left/right) of the image -->
-    <
-    div id = "text" >
-    <
-    h1 id = "modal-title" > < /h1> <
-    p id = "modal-text" > < /p> <
-    /div> <
-    div id = "image" >
-    <
-    img id = "modal-image"
-src = ""
-alt = "" >
-    <
-    /div> <
-    /div> <
-    /div> <
-    /div> <
-    /head> <
-    /body>
-
-<
-/html>     };
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target === modal) {
-    hidemodal();
-}
-};
-});
-// When the user clicks on <closeBtn> (x), close the modal
-closeBtn.addEventListener('click', function(evt) {
-    hidemodal();
-});
-//Function to hide the modal
-function hidemodal() {
-    modal.style.display = "none";
-    closeBtn.style.display = "none";
-}
-
-}
+    }
 });
